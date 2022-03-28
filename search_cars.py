2d94db691bef828112bea34e1413396a98b96bdc
jeanmichelneto <jeanmichelneto@gmail.com>
2022-03-28 11:31:31 -0300

    second commit

--- search_cars.py
+++ search_cars.py
@@ -1,9 +1,19 @@
-list_cars = open('cars.txt')
+
 # print(list_cars.read())
 # print(list_cars.readline())
 # print(list_cars.readline())
 
 # print(list_cars.readlines()[0])  # prints only the first item
+def cars_number():
+    list_cars = open('cars.txt')
+    name_car = list_cars.readlines()[0]
+    # print(name_car[3:18])
+    list_cars.close()  # close this file
+    list_customers = open('customers.txt')
 
-name_car = list_cars.readlines()[5]
-print(name_car[3:18])
+    for customers in list_customers:
+        print('Hello', customers,
+              'The best selling car was yours in 2021', name_car[3:18], 'congratulations')
+
+
+cars_number()
