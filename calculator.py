3a5c0176665bcdfeb15432badf2b579b84afe133
jeanmichelneto <jeanmichelneto@gmail.com>
2022-04-03 00:23:55 -0300

    new commit

---
+++ calculator.py
@@ -0,0 +1,15 @@
+n1 = int(input('Digite o Primeiro Numero: '))
+sinal = input('Digite o Sinal da Operação: ')
+n2 = int(input('Digite o segundo numero '))
+
+if sinal == "+":
+    print(n1+n2)
+
+elif sinal == "-":
+    print(n1-n2)
+
+elif sinal == "*":
+    print(n1*n2)
+
+elif sinal == "/":
+    print(n1/n2)
---
+++ list.py
@@ -0,0 +1,2 @@
+my_list = [3, 5, 1, 4]
+print(sorted(my_list))
